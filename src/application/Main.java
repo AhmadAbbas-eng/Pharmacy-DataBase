3188079a0881b055698b334a24363d4bcfc41b2a
Aseel-Sabri <aseel.k.sabri@gmail.com>
2022-01-23 15:11:53 +0200

    ****

--- src/application/Main.java
+++ src/application/Main.java
@@ -28,7 +28,7 @@ public class Main extends Application {
 
 	public static void main(String[] args) throws IOException, ClassNotFoundException, SQLException, ParseException {
 
-		System.out.println("Aseel .......... is Readingd a");
+		System.out.println("Aseel ***** is Readingd a");
 		Main.readingData();
 		launch(args);
 	}
