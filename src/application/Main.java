3d639cd6a14088097737c20bac8f551115d2c53f
Ahmad Abbas <1190245@student.birzeit.edu>
2022-01-23 12:58:43 +0200

    Trial 2

--- src/application/Main.java
+++ src/application/Main.java
@@ -28,7 +28,7 @@ public class Main extends Application {
 
 	public static void main(String[] args) throws IOException, ClassNotFoundException, SQLException, ParseException {
 
-		System.out.println("Ahmad is Reading ");
+		System.out.println("Ahmad is Reading a");
 		Main.readingData();
 		launch(args);
 	}
