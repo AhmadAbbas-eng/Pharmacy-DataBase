ccd3773c7bc6acb272cb0155191ed0731996ba25
LoorSawalhi <loorwael@yahoo.com>
2022-01-23 14:12:52 +0200

    loor..

--- src/application/Main.java
+++ src/application/Main.java
@@ -28,7 +28,7 @@ public class Main extends Application {
 
 	public static void main(String[] args) throws IOException, ClassNotFoundException, SQLException, ParseException {
 
-		System.out.println("weeeeeee fis Readingd a");
+		System.out.println("loor fis Readingd a");
 		Main.readingData();
 		launch(args);
 	}
