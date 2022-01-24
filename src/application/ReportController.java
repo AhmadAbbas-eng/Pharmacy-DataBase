d4c87789ecb79a70c949d92ade2dcf7619337c2f
Aseel-Sabri <aseel.k.sabri@gmail.com>
2022-01-25 01:31:43 +0200

    Reports2

--- src/application/ReportController.java
+++ src/application/ReportController.java
@@ -25,20 +25,14 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.StackPane;
 import javafx.stage.DirectoryChooser;
 
 public class ReportController implements Initializable {
-	@FXML
-	private Button choosePathButton;
 
 	@FXML
 	private StackPane display;
-
-	@FXML
-	private TextField pathTextField;
 
 	@FXML
 	private ComboBox<String> reportType;
