b6d230d690983c24b9f0dea339a10afe145b07ae
LoorSawalhi <loorwael@yahoo.com>
2022-01-25 23:05:58 +0200

    .

--- src/application/MainPageController.java
+++ src/application/MainPageController.java
@@ -1,24 +1,24 @@
 package application;
 
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.fxml.Initializable;
-import javafx.scene.layout.Region;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
-
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import Relations.Employee;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
 public class MainPageController implements Initializable {
+    
 	@FXML
 	private Label choosenButtonLabel;
 
@@ -191,7 +191,6 @@ public class MainPageController implements Initializable {
 
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
-
 		if (Employee.getCurrentID() != 0) {
 			userNameLabel.setText(Employee.getEmployeeName());
 		} else {
