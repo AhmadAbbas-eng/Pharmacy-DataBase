20489cad7ca8e17ba412aee9a9ad42e0fd6b2bca
Ahmad Abbas <1190245@student.birzeit.edu>
2022-02-04 14:44:45 +0200

    Fix module

--- src/module-info.java
+++ src/module-info.java
@@ -2,7 +2,6 @@ module Database {
 	requires transitive javafx.controls;
 	requires transitive javafx.fxml;
 	requires transitive java.sql;
-	requires transitive mysql.connector.java;
 	requires java.desktop;
 	requires java.management;
 	requires javafx.graphics;
