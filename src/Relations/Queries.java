3e5eb91c225a75eb7aa4172f3fc2b99c5e5f9dba
Ahmad Abbas <1190245@student.birzeit.edu>
2022-02-12 11:17:16 +0200

    AGAIN ... I don't know

--- src/Relations/Queries.java
+++ src/Relations/Queries.java
@@ -1,9 +1,14 @@
 package Relations;
 
-import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
