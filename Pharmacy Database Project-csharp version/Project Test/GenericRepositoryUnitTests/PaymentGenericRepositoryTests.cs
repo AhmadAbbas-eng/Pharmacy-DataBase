253e75529390eb640f91690753f64e3ffa3bf7d8
Ahmad Abbas <73943807+ahmad-m-abbas@users.noreply.github.com>
2023-11-04 16:04:35 +0200

    Adding Payment Repo Test

---
+++ Pharmacy Database Project-csharp version/Project Test/GenericRepositoryUnitTests/PaymentGenericRepositoryTests.cs
@@ -0,0 +1,34 @@
+﻿using Domain.Models;
+using Infrastructure;
+using Infrastructure.Entities;
+using Infrastructure.Repository;
+
+namespace Project_Test.GenericRepositoryUnitTests;
+
+public class PaymentGenericRepositoryTests : BaseGenericRepositoryTests<PharmacyDbContext, Payment, PaymentDomain, int>
+{
+    public PaymentGenericRepositoryTests()
+    {
+        _repository = new Repository<Payment, PaymentDomain, int>(_context, _mapper);
+    }
+
+    [Fact]
+    public void FindProducts_ReturnsCorrectProducts()
+    {
+        var product = CreateDomainModel();
+        _repository.Add(product);
+        _repository.Save();
+
+        var foundProducts = _repository.Find(p => p.PaymentAmount == product.PaymentAmount);
+
+        Assert.Contains(foundProducts, p => p.PaymentAmount == product.PaymentAmount);
+    }
+
+    [Fact]
+    public void FindProducts_ReturnsEmpty_WhenPredicateDoesNotMatch()
+    {
+        var foundProducts = _repository.Find(p => p.PaymentAmount == -1);
+
+        Assert.Empty(foundProducts);
+    }
+}
